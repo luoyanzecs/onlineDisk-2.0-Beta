e4760f535344e84c3e92bdef43967ca434c380bc
luoyanzecs <1421286905@qq.com>
2021-02-01 13:16:28 +0800

    web

--- web-project/src/components/content/File.vue
+++ web-project/src/components/content/File.vue
@@ -9,8 +9,9 @@
         <div>
           <span @click="unFold(item.ser)" class="fold">{{item.ser}}</span>
           <div :ref="item.ser">
-            <div v-for="file in item.files" class="file-name">
-              <span>{{file}} </span>
+            <div v-for="file in item.files" class="file-item">
+              <span v-if="file.isDir" @click="getFilesList(file.filename)"><img src="../../assets/icon/wenjianjia.png"/></span>
+              <span class="file-font">{{file.filename}} </span>
               <span @click="download(file)"><img src="../../assets/icon/xiazai.png"/></span>
               <span @click="deleteFile(item.ser, file)"><img src="../../assets/icon/shanchu.png"/></span>
               <span><img src="../../assets/icon/fenxiang.png"/></span>
@@ -32,8 +33,8 @@
       return {
         items: [
           //test data
-          {ser: 'A', files: ['filename1------------------------------------', 'filename2', '3']},
-          {ser: 'B', files: ['filename3', 'filename4']},
+          {ser: 'A', files: [{filename: 'filename1', isDir: true}, {filename: 'filename2', isDir: false}, {filename: '333333', isDir: false}]},
+          {ser: 'B', files: [{filename: '333333', isDir: false}, {filename: '44444', isDir: false}]},
         ],
       }
     },
@@ -44,7 +45,7 @@
       //asynchronous get RSA_PUBLIC_KEY_CLIENT,
       // and create RSA_PUBLIC_KEY_SERVER, RSA_PRIVATE_KEY_SERVER with vuex actions,
       // request()
-      this.getFilesList();
+      this.getFilesList('home');
     },
     methods: {
       unFold(ser) {
@@ -52,14 +53,15 @@
         style.display = (style.display == '') || (style.display == 'block')
           ? 'none' : 'block'
       },
-      getFilesList() {
+      getFilesList(dir) {
         //check use rsa or not {$store.state.isRsa}. encryption or not
         //if use rsa send {$store.state.RSA_PUBLIC_KEY_SERVER} to server for encryption
         request({
-          //url: /file/download/filenames/useRsa?/RSA_PUBLIC_KEY_SERVER
+          //url: /file/download/home
         }).then(res => {
           // if encryption use {$store.state.RSA_PRIVATE_KEY_SERVER} to decrypt.
           // res: "A":["filename1","filename2"],"B":["filename3", "filename4"]
+          this.items = JSON.parse(res.data);
         }).catch(err => {
           //print err info
         })
